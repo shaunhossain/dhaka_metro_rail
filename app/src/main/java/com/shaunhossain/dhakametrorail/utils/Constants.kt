7c0ba9d246c20366b019391cb9318bfa13c8a599
shaunhossain <shaunhossain95@gmail.com>
2023-06-18 16:26:34 +0600

    remove map key

--- app/src/main/java/com/shaunhossain/dhakametrorail/utils/Constants.kt
+++ app/src/main/java/com/shaunhossain/dhakametrorail/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.shaunhossain.dhakametrorail.utils
 object Constants {
 
     //BariKoi mapView
-    private const val mapTilerKey: String = "NDE2NzpVNzkyTE5UMUoy"
+    private const val mapTilerKey: String = "add your barikoi map api key"
     //const val STYLE_URL = "https://map.barikoi.com/styles/barikoi-bangla/style.json?key=${mapTilerKey}"
     const val STYLE_URL = "https://geoserver.bmapsbd.com/styles/barikoi-custom-traffic/style.json?key=${mapTilerKey}"
 }
